05509dc9af395696da45a0622be04bce64811014
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2023-12-12 09:40:58 -0800

    Banner section created

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,15 +1,34 @@
-import animated from '../../../assets/images/animated.gif'
+import reactLogo from '../../../assets/react.svg'
+import '../../../../src/App.css'
+
 
 const Banner = () => {
     return (
-        <div className="hero h-[500px]" style={{ backgroundImage: `url(${animated})` }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Sh</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
-                </div>
+        <div>
+            <div className='flex justify-end p-2 lg:p-4'>
+                <a>
+                </a>
+                <a href="https://react.dev" target="_blank" className=''>
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <div>
+                <span className='satisfyFont text-center text-7xl space-y-7'>
+                    <p className='font-semibold'>I am a MERN Stack</p>
+                    <p className='font-semibold'>Web developer</p>
+                </span>
+                <span>
+                    <p className='text-center mt-12 text-xl text-gray-700'>Hi, My name is Shahidul Islam. <br />
+                        I am a MERN stack web developer. I want to design user interactive & beautiful website.<br/> Weclome to my portfolio page that I have designed to show my skills and expertise.
+                    </p>
+                </span>
+            </div>
+            <div className='flex justify-start p-2 lg:p-4'>
+                <a>
+                </a>
+                <a href="https://react.dev" target="_blank" className=''>
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
             </div>
         </div>
     );
